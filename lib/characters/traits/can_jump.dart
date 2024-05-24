52b4fd5354a2398e6f14ea1edde67f289735d280
pemix09 <pemix09@onet.pl>
2024-05-24 15:59:00 +0200

    double jump different approach

--- lib/characters/traits/can_jump.dart
+++ lib/characters/traits/can_jump.dart
@@ -1,13 +1,14 @@
-import 'package:flame/components.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_adventure/characters/playable_character.dart';
 
 mixin CanJump on PlayableCharacter {
-  final double jumpForce = 260;
-  int get maxJumps => 2;
+
   int queuedJumps = 0;
   int madeJumps = 0;
+
+  double get jumpForce => 260;
+  int get maxJumps => 2;
 
   void jump() {
     if (queuedJumps < maxJumps) {
@@ -15,8 +16,10 @@ mixin CanJump on PlayableCharacter {
     }
   }
 
+  // tutaj to się wykonuje podczas renderowania kadej klatki, dlatego ustawia od razu na 2 madeJumps
   void checkShouldJump(double dt) {
-    if (queuedJumps <= 0 || madeJumps == maxJumps) {
+    if (queuedJumps <= 0)    {
+      debugPrint('queued: $queuedJumps');
       return;
     }
 
@@ -25,7 +28,7 @@ mixin CanJump on PlayableCharacter {
     }
     velocity.y = -jumpForce;
     position.y += velocity.y * dt;
-    madeJumps++;
+    queuedJumps--;
   }
 
   void resetJumps() {
