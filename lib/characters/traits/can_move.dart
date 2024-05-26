1af545455364449b24ed3a6d8497979fd3d7a3b4
pemix09 <pemix09@onet.pl>
2024-05-26 18:34:49 +0200

    can move mixin visibility fix

--- lib/characters/traits/can_move.dart
+++ lib/characters/traits/can_move.dart
@@ -2,24 +2,24 @@ import 'package:flame/game.dart';
 import 'package:pixel_adventure/characters/character.dart';
 
 mixin CanMove on Character {
-  double horizontalMovement = 0;
+  double _horizontalMovement = 0;
   double moveSpeed = 100;
   Vector2 velocity = Vector2.zero();
 
   void moveRight() {
-    horizontalMovement = 1;
+    _horizontalMovement = 1;
   }
 
   void moveLeft() {
-    horizontalMovement = -1;
+    _horizontalMovement = -1;
   }
 
   void resetHorizontalMove() {
-    horizontalMovement = 0;
+    _horizontalMovement = 0;
   }
 
   void checkHorizontalMove(double dt) {
-    velocity.x = horizontalMovement * moveSpeed;
+    velocity.x = _horizontalMovement * moveSpeed;
     position.x += velocity.x * dt;
   }
 }
